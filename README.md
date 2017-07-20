ec5f08703d42f6cae245df4a89f39af6627dd5a7
ZhangBaoCai <talent518@yeah.net>
2017-07-20 21:13:33 +0800

    添加需要训练的图片和训练代码retrain。

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# tensorflow
+# tensorflow for 1.2.1+
 收集完成的tensorflow实例，使用图片分类模式训练并使用图片识别，支持控制台模式和B/S模式。
+  * retrain.py 把图片分类并生成识别数据库文件
+  * retrain.sh 执行retrain.py的样例命令
---
+++ retrain.sh
@@ -0,0 +1,1 @@
+python retrain.py --bottleneck_dir bottleneck --how_many_training_steps 4000 --model_dir model --output_graph output_graph.pb --output_labels output_labels.txt --image_dir train_images/
