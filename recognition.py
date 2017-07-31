858e7ad6aee868bff60d922ce8a7162e03bef3c7
ZhangBaoCai <talent518@yeah.net>
2017-07-31 16:33:19 +0800

    识别时添加准确率计算代码。

--- recognition.py
+++ recognition.py
@@ -17,8 +17,10 @@ with tf.gfile.FastGFile("output_graph.pb", 'rb') as f:
 with tf.Session() as sess:
     softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
 
+    rights = 0.0
     for image_file in sys.argv[1:]:
         print("===========================\n%s\n===========================" % os.path.basename(image_file))
+        nam = os.path.basename(image_file).split('-')[0]
  
         # 读取图像
         image = tf.gfile.FastGFile(image_file, 'rb').read()
@@ -26,7 +28,12 @@ with tf.Session() as sess:
      
         # 根据分类概率进行排序
         top = predict[0].argsort()[-len(predict[0]):][::-1]
+        if labels[top[0]] == nam:
+            rights += 1.0
         for index in top:
             human_string = labels[index]
             score = predict[0][index]
             print(human_string, score)
+
+    size = len(sys.argv)-1
+    print("===========================\nresult: rights(%d) * 100.0 / size(%d) = %.2f%%" % (rights, size, rights * 100.0 / float(size)))
